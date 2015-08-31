d4a53fecba51ebdcc6ed79e12e340409fbe95f77
crossjs <liwenfu@crossjs.com>
2015-09-01 00:02:51 +0800

    add comment param

---
+++ index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+// React component
+class Counter extends React.Component {
+  render(){
+    const { value, onIncreaseClick } = this.props;
+    return (
+      <div>
+        <span>{value}</span>
+        <button onClick={onIncreaseClick}>Increase</button>
+      </div>
+    );
+  }
+}
+
+// Action:
+const increaseAction = {type: 'increase'};
+
+// Reducer:
+function counter(state={count: 0}, action) {
+  let count = state.count;
+  switch(action.type){
+    case 'increase':
+      return {count: count+1};
+    default:
+      return state;
+  }
+}
+
+// Store:
+let store = createStore(counter);
+
+// Map Redux state to component props
+function mapStateToProps(state)  {
+  return {
+    value: state.count
+  };
+}
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncreaseClick: () => dispatch(increaseAction)
+  };
+}
+
+// Connected Component:
+let App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+React.render(
+  <Provider store={store}>
+    {() => <App />}
+  </Provider>,
+  document.getElementById('root')
+);
