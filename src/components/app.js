3d7bc03f14c8f5ecfb785808ec61280221d1856b
crossjs <liwenfu@crossjs.com>
2015-10-30 01:14:54 +0800

    upgrade dependencies

--- src/components/app.js
+++ src/components/app.js
@@ -17,7 +17,7 @@ export default class App extends Component {
         <Provider key="provider" store={store}>
           <Routes />
         </Provider>
-        <Debugger key="debugger" store={store} />
+        { __DEVTOOLS__ && <Debugger key="debugger" store={store} /> }
       </div>
     )
   }
