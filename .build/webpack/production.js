613869f8ec0b346d7bd68a7f41579769649ee425
crossjs <liwenfu@crossjs.com>
2015-12-02 17:46:56 +0800

    update compile configuration

--- .build/webpack/production.js
+++ .build/webpack/production.js
@@ -1,26 +1,30 @@
-import webpack           from 'webpack';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import webpackConfig     from './_base';
+import webpack from 'webpack'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import webpackConfig from './_base'
+
+import config from '../config'
+
+const dist = config.get('dir_dist')
 
 webpackConfig.module.loaders = webpackConfig.module.loaders.map(loader => {
   if (/css/.test(loader.test)) {
-    const [first, ...rest] = loader.loaders;
+    const [first, ...rest] = loader.loaders
 
-    loader.loader = ExtractTextPlugin.extract(first, rest.join('!'));
-    delete loader.loaders;
+    loader.loader = ExtractTextPlugin.extract(first, rest.join('!'))
+    delete loader.loaders
   }
 
-  return loader;
-});
+  return loader
+})
 
 webpackConfig.plugins.push(
-  new ExtractTextPlugin('[name].css'),
+  new ExtractTextPlugin(`${dist}/[name].[chunkhash].css`),
   new webpack.optimize.UglifyJsPlugin({
-    compress : {
-      'unused'    : true,
-      'dead_code' : true
+    compress: {
+      unused: true,
+      dead_code: true
     }
   })
-);
+)
 
-export default webpackConfig;
+export default webpackConfig
