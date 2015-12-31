115cf1138e190b508453ff661e4edd8252cf1830
crossjs <liwenfu@crossjs.com>
2016-01-01 04:05:12 +0800

    handle diff time between server and client

--- src/rdx/reducers/tokens.js
+++ src/rdx/reducers/tokens.js
@@ -2,9 +2,15 @@ import { handleActions } from 'redux-actions'
 
 export default handleActions({
 
-  LOGIN: (state, action) => ({
-    ...state, ...action.payload
-  }),
+  LOGIN: (state, action) => {
+    const diff = new Date(action.payload.expires_at) - Date.now()
+
+    return {
+      ...state,
+      diff,
+      ...action.payload
+    }
+  },
 
   LOGOUT: (state, action) => ({
     ...action.payload
