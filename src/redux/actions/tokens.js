cf2129e640fc5dbc89a2c29c1bd2ff0cb97c693d
kagawagao <kingsongao1221@gmail.com>
2016-03-23 00:31:55 +0800

    see CHANGELOG.md v0.1.2

--- src/redux/actions/tokens.js
+++ src/redux/actions/tokens.js
@@ -4,6 +4,12 @@ import Tokens from '../models/tokens'
 
 export const login = createAction('LOGIN',
   payload => new Tokens().POST(payload))
+  // 如果你升级了redux-promise 到最新版本，请使用下面的代码来返回数据
+  // 同时在utils/rest.js里对axios 直接返回一个Promise, 见 line:247
+  // async payload => {
+  //   const result = await new Tokens().POST(payload)
+  //   return result.data
+  // })
 
 export const logout = createAction('LOGOUT',
   payload => new Tokens().DELETE(payload)
